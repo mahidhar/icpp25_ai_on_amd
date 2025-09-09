a17aca98f218b3e459a92f85cf745ce7e097b186
mahidhar <mahidhar@users.noreply.github.com>
2025-09-08 18:35:47 -0700

    Add files via upload

---
+++ jax-test.py
@@ -0,0 +1,10 @@
+import jax
+import jax.numpy as jnp
+
+print(jax.devices())
+def tanh(x):
+  y = jnp.exp(-2.0 * x)
+  return (1.0 - y) / (1.0 + y)
+
+grad_tanh = jax.grad(tanh)
+print(grad_tanh(1.0))
